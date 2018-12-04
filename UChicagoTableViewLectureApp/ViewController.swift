1c9dd92cdde4a3170c4b46e8d6c4ffd9d7a0b951
chelseatroy <chelsea.dommert@gmail.com>
2018-12-03 23:31:54 -0600

    Initial Commit

---
+++ UChicagoTableViewLectureApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UChicagoTableViewLectureApp
+//
+//  Created by Chelsea Dommert on 12/3/18.
+//  Copyright © 2018 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
